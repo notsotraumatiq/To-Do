bbd968340418e32476ad3c1b3662c899e7e58c65
notsotraumatiq <atiqpatel95@gmail.com>
2022-10-11 13:47:45 -0400

    changes

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,8 +1,11 @@
-import * as React from 'react';
-import styles from './index.module.css';
-const Input = ({placeholder}) => {
-    return (<div className={styles.inputField}><input placeholder={placeholder}>
-    </input></div>  );
-}
+import * as React from "react";
+import styles from "./index.module.css";
+const Input = ({ placeholder }) => {
+  return (
+    <div className={styles.inputField}>
+      <input className={styles.input} placeholder={placeholder} />
+    </div>
+  );
+};
 
 export default Input;
