ebb8759ad0ca552430bdd0fb200638d4f32f3e4c
notsotraumatiq <atiqpatel95@gmail.com>
2023-05-13 10:35:09 -0400

    added changes

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Onboarding from "./components/Onboarding/Onboarding";
+import List from "./components/List";
 
 export const App = () => {
   return (
@@ -9,7 +10,7 @@ export const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/Onboarding' element={<Onboarding />} />
         <Home /> */}
-      <Onboarding />
+      {/* <Onboarding /> */}
       {/* </Routes> */}
     </div>
   );
