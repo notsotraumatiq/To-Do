39bc73d4e9372ce8c0fb7fed933faf570a66ce2b
notsotraumatiq <atiqpatel95@gmail.com>
2023-05-13 10:51:15 -0400

    added list

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import List from "./components/List";
 
 export const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <List />
+    </div>
+  );
 };
---
+++ src/components/List/index.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+const List = () => {
+  const [items, setItems] = useState<string[]>([]);
+  const [newItem, setNewItem] = useState<string>("");
+  const [editIndex, setEditIndex] = useState<number | null>(null);
+
+  function handleEditItem(i: number, value: string) {
+    const updatedItems = [...items];
+    updatedItems[i] = value;
+    setItems(updatedItems);
+    setEditIndex(null);
+  }
+  return (
+    <div>
+      <h1>List</h1>
+      <input
+        type="text"
+        placeholder="Add item"
+        onChange={(event) => setNewItem(event.target.value)}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            setItems([...items, newItem]);
+            setNewItem("");
+          }
+        }}
+        value={newItem}
+      />
+      {items.length > 0 ? (
+        <ul>
+          {items.map((item, i) =>
+            editIndex === i ? (
+              <li>
+                <input
+                  autoFocus
+                  value={item}
+                  onChange={(event) => {
+                    const updatedItems = [...items];
+                    updatedItems[i] = event.target.value;
+                    setItems(updatedItems);
+                  }}
+                  onKeyDown={(event: any) =>
+                    event.key === "Enter" &&
+                    handleEditItem(i, event.target.value)
+                  }
+                ></input>
+              </li>
+            ) : (
+              <li key={i} onClick={() => setEditIndex(i)}>
+                {item}
+              </li>
+            ),
+          )}
+        </ul>
+      ) : (
+        <p>there are no items in the list </p>
+      )}
+    </div>
+  );
+};
+
+export default List;
